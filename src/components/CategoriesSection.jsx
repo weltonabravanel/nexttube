97640888258c38847dacb6db61b8ed582cfa431c
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-07 16:04:17 -0300

    Update CategoriesSection.jsx

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -7,11 +7,11 @@ const CategoriesSection = () => {
     <div className="xl:flex items-center xl:max-w-[60%] shrink max-w-[40%] hidden  overflow-x-scrol space-x-14 scrollbar-hide md:pl-0 pl-5 md:pr-5">
     <Link to="/"><Categories title="All" id='' handleClick="" /></Link>
     <Categories title="Trending" id="Trending" handleClick="" />
-    <Categories title="Music" id="music"  handleClick="" />
-    <Categories title="News" id="news" handleClick="" />
-     <Categories title="Gaming" id="gaming" handleClick="" />
-    <Categories title="Sports" id="sports" handleClick="" />
-    <Categories title="Films" id="movies" handleClick="" />
+    <Categories title="Musicas" id="music"  handleClick="" />
+    <Categories title="Notícias" id="news" handleClick="" />
+     <Categories title="Games" id="gaming" handleClick="" />
+    <Categories title="Esportes" id="sports" handleClick="" />
+    <Categories title="Filmes" id="movies" handleClick="" />
     <Categories title="Fashion" id="26" handleClick="" />
     {/* <Categories title="Comedy" id="23" handleClick="" /> */}
     {/* <Categories title="DOTA" id="20" handleClick="" /> */}
