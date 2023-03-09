b13832fa0002032737c1772a265969030609e6d0
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-09 18:13:23 -0300

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,3 +1,31 @@
+import React from 'react'
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import Loader from './Loader'
+
+const VideoCard = ({videoDetails}) => {
+  if(!videoDetails) return <Loader/>
+  
+
+  return (
+    <div className="videoCard shadow-lg">
+    {/* card media is here  */}
+  <Link to={`/video/${videoDetails?.id?.videoId}`}><figure><img src={videoDetails?.snippet?.thumbnails?.medium?.url}  alt={videoDetails?.snippet?.channelTitle} className='w-100' /></figure></Link>
+    
+     {/* card content  is here  */}
+    <Link to={`/video/${videoDetails?.id?.videoId}`}><p className='fw-bold video-title'>{videoDetails?.snippet?.title.slice(0,60)}</p></Link>
+    
+    <Link to={`/chenel/${videoDetails?.snippet?.channelId}`}>
+    <span className='chenel-title'>{videoDetails?.snippet?.channelTitle}</span> 
+    <AiFillCheckCircle color='gray' size={15} className="ms-1"/>
+    </Link>
+
+    </div>
+  )
+}
+
+export default VideoCard
+
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import checkicon from "../assets/check.svg";
