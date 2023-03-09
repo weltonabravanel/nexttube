9845cb092e06eebce6acb9ac8645122917c8cf33
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-09 20:06:26 -0300

    Update SideBar.jsx

--- src/containers/SideBar.jsx
+++ src/containers/SideBar.jsx
@@ -107,7 +107,29 @@ const SideBar = () => {
                     <path d="M14 7l7 0l0 7"></path>
                   </svg>
                 }
-                title="Trending"
+                title="Shows"
+              />
+              </Link>
+              <SideBarItem
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler icon-tabler-trending-up"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M3 17l6 -6l4 4l8 -8"></path>
+                    <path d="M14 7l7 0l0 7"></path>
+                  </svg>
+                }
+                title="Tendências"
               />
               <SideBarItem
                 icon={
